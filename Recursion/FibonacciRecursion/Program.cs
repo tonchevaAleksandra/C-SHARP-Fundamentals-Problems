408dfeae89da8fd0909cb250f08a72e8d57a0907
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-08-05 18:30:40 +0300

    Add files

---
+++ Recursion/FibonacciRecursion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+
+namespace FibonacciRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter a positive number to receive the fibonacci number!");
+            int n = int.Parse(Console.ReadLine());
+            int result = Fibonacci(n);
+            Console.WriteLine(result);
+        }
+        private static int Fibonacci(int n)
+        {
+            if(n<2)
+            {
+                return n;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+    }
+}
---
+++ Recursion/RecursionExample1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.Common;
+using System.Numerics;
+
+namespace RecursionExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello enter the number to receive his factorial!");
+            int n = int.Parse(Console.ReadLine());
+            if(n<=0)
+            {
+                Console.WriteLine("Please enter a possitive number!");
+                n = int.Parse(Console.ReadLine());
+            }
+            BigInteger factorial = 0;
+            factorial = Factorial(n);
+            Console.WriteLine($"The factorial of {n} is {factorial}");
+        }
+        private static BigInteger Factorial(int factorialNum)
+        {
+            
+            if (factorialNum == 1)
+                return 1;
+            return factorialNum * Factorial(factorialNum - 1);
+        }
+    }
+}
